c952989df87a444ae8b0593144e2390c976a519d
maxxfuu <maxfu826@gmail.com>
2026-07-03 20:55:52 -0700

    finally seperated decode and prefill, benchmark tooling working as well

--- src/ridgeline/benchmarks/compute_roof.py
+++ src/ridgeline/benchmarks/compute_roof.py
@@ -1,15 +1,15 @@
-import torch 
-import ridgeline.measurement.timing as timing
+import torch
+from ridgeline.measurement.benchmark import benchmark
 
 def compute_roof(N: int, device: str) -> float:
-  # pre-allocate tensors 
+  # pre-allocate tensors
   a = torch.randn(N, N, dtype=torch.float16, device=device)
   b = torch.randn(N, N, dtype=torch.float16, device=device)
   c = torch.empty(N, N, dtype=torch.float16, device=device)
 
-  # only measure matmul compute time, not memory bandwidth 
+  # only measure matmul compute time, not memory bandwidth
   make_op = lambda: torch.matmul(a, b, out=c)
-  median_time = timing.benchmark(make_op, device)
+  median_time = benchmark(make_op, device)
 
   print(f"FLOPs: {2 * N**3}")
   print(f"median time: {median_time} seconds")
@@ -31,10 +31,10 @@ total = ~400MB
 """
 
 def main():
-  device, N_List = "mps", [2048, 4096, 6144, 8192]
+  device, sizes = "mps", [2048, 4096, 6144, 8192]
   results = list()
 
-  for N in N_List:
+  for N in sizes:
     achieved = compute_roof(N, device)
     results.append((N, achieved))
 
--- src/ridgeline/benchmarks/memory_roof.py
+++ src/ridgeline/benchmarks/memory_roof.py
@@ -1,26 +1,26 @@
-import torch 
-import ridgeline.measurement.timing as timing
+import torch
+from ridgeline.measurement.benchmark import benchmark
 
 def memory_roof(N: int, device: str) -> float:
-  # pre-allocate tensors 
+  # pre-allocate tensors
   x = torch.randn(N, dtype=torch.float32, device=device)
   y = torch.empty(N, dtype=torch.float32, device=device)
 
   # measure time it takes to move data from x to y
   make_op = lambda: torch.add(x, 1, out=y)
-  median_time = timing.benchmark(make_op, device)
+  median_time = benchmark(make_op, device)
 
   print(f"Bytes: {2 * 4 * N}")
   print(f"Median Time: {median_time} seconds")
-  print(f"GBs/second: {(2 * 4 * N / median_time / 1e9).round(2)} GBs/second \n") 
+  print(f"GBs/second: {(2 * 4 * N / median_time / 1e9).round(2)} GBs/second \n")
 
-  return (2 * 4 * N) / median_time  
+  return (2 * 4 * N) / median_time
 
 def main():
-  device, N_List = "mps", [20000000, 50000000, 100000000, 200000000, 400000000, 800000000]
+  device, sizes = "mps", [20000000, 50000000, 100000000, 200000000, 400000000, 800000000]
   results = list()
 
-  for N in N_List:
+  for N in sizes:
     achieved = memory_roof(N, device)
     results.append((N, achieved))
 
